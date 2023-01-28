e335e153fa9988150f01dfc97e7e894291ed829c
Andra <94204279+asteele1493@users.noreply.github.com>
2023-01-28 14:12:41 -0800

    Added CC scaffolding

---
+++ javascript/trees/tree-implementation/tree-imp.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//specifically a node that has a .left and a .right
+//different structure of node than what we've been working with
+class btNode{
+  constructor(value, left, right){
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinaryTree{
+  constructor(root){
+    this.root = root;
+  }
+
+//pre-order
+// root left right(left first is the way)
+preOrder(){
+  let traversal = [];
+
+}
+
+//post-order
+
+//in-order
+
+
+}
