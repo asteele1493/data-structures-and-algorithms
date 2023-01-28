8a0ed29ae104eb0f867d9cc982982700a94c1b95
Andra <94204279+asteele1493@users.noreply.github.com>
2023-01-28 14:18:04 -0800

    Tree scaffolding

--- javascript/trees/tree-implementation/tree-imp.js
+++ javascript/trees/tree-implementation/tree-imp.js
@@ -1,30 +1,42 @@
-'use strict';
+"use strict";
 
 //specifically a node that has a .left and a .right
 //different structure of node than what we've been working with
-class btNode{
-  constructor(value, left, right){
+class btNode {
+  constructor(value, left, right) {
     this.value = value;
     this.left = left;
     this.right = right;
   }
 }
 
-class BinaryTree{
-  constructor(root){
+class BinaryTree {
+  constructor(root) {
     this.root = root;
   }
 
-//pre-order
-// root left right(left first is the way)
-preOrder(){
-  let traversal = [];
+  //pre-order
+  // root left right(left first is the way)
+  preOrder() {
+    let traversal = [];
+    traversal.push(this.value); //root?
 
-}
+    if (this.left) {
+      let leftTraversal = this.left.preOrder();
+      traversal = traversal.concat(leftTraversal);
+    }
 
-//post-order
+    if (this.right) {
+      let rightTraversal = this.right.preOrder();
+      traversal = traversal.concat(rightTraversal);
+    }
 
-//in-order
+    return traversal;
+  }
 
+  //post-order
+  //left right root
 
+  //in-order
+  //left root right
 }
